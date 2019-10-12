33bbaec81412445284a6c8e3af82fb9c1e975e64
ReinaldyRabbany <reinaldy_rabbany@yahoo.com>
2019-10-12 23:34:33 +0700

    re-commit for feat: tambah obat

--- sibat/src/main/java/apap/tugaspemrograman/sibat/service/ObatService.java
+++ sibat/src/main/java/apap/tugaspemrograman/sibat/service/ObatService.java
@@ -3,6 +3,7 @@ package apap.tugaspemrograman.sibat.service;
 import apap.tugaspemrograman.sibat.model.ObatModel;
 import java.util.List;
 
+// Service for Obat
 public interface ObatService {
     void addObat(ObatModel obat);
     List<ObatModel> getListObat();
