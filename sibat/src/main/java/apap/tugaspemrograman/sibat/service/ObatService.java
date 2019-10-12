5dd588a49ad6ff643509131282dcb5937eefaf53
ReinaldyRabbany <reinaldy_rabbany@yahoo.com>
2019-10-13 00:40:42 +0700

    feat: detail view obat

--- sibat/src/main/java/apap/tugaspemrograman/sibat/service/ObatService.java
+++ sibat/src/main/java/apap/tugaspemrograman/sibat/service/ObatService.java
@@ -2,10 +2,13 @@ package apap.tugaspemrograman.sibat.service;
 
 import apap.tugaspemrograman.sibat.model.ObatModel;
 import java.util.List;
+import java.util.Optional;
 
 // Service for Obat
 public interface ObatService {
     void addObat(ObatModel obat);
     List<ObatModel> getListObat();
     String generateKode(ObatModel obat);
+    Optional<ObatModel> getObatByNoRegistrasiObat(String noReg);
+    String convertIdJenisToString(Long idJenis);
 }
